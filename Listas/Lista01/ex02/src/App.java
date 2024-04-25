723909a8a3c50be2902f81e6b69d378748dedd94
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-04-25 20:40:03 -0300

    Lista 1

---
+++ Listas/Lista01/ex02/src/App.java
@@ -0,0 +1,30 @@
+/*import java.util.*;
+public class App {
+    private static final Scanner SCAN = new Scanner(System.in);
+    private static final int TAMANHO_MAPA = 4;
+    public static void main(String[] args) throws Exception {
+        
+    }
+    public static void percorrerMatriz (int[][] mapa){
+    boolean[][] matriz = new boolean[TAMANHO_MAPA][TAMANHO_MAPA];        
+        
+    for(int i = 0; i < TAMANHO_MAPA; i++){
+            for(int j = 0; j < TAMANHO_MAPA; j++){
+                matriz[i][j] = false;
+
+            }
+        }
+    }
+
+    public static void receberParametro (int[][] mapa){
+        for(int i = 0; i < TAMANHO_MAPA; i++){
+            for(int j = 0; j < TAMANHO_MAPA; j++){
+                percorrerMatriz(int[][] );
+            }
+        }
+    }
+}
+
+Não consegui terminar e o unico dia q eu tinha disponivel para monitoria não teve... i'm Sorry
+"*Emoji triste capenga xoxo chorão*" :c
+*/ 
